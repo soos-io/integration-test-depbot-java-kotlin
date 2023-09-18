f47a677e2713be0c25054749ab4373ae536ef09a
SOOS-GSteen <gsteen@soos.io>
2023-09-18 15:23:02 -0400

    c

---
+++ src/build.gradle.kts
@@ -0,0 +1,36 @@
+rootProject.name = 'DepTest'
+version = "0.2"
+
+plugins {
+    id("demo.kotlin-application-conventions")
+}
+
+val creationDate by extra { project.findProperty("creationDate") ?: LocalDate.now().toString() }
+val javaVersion = JavaVersion.VERSION_11
+configure<JavaPluginConvention> {
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+}
+
+kotlinDslPluginOptions {
+    jvmTarget.set(javaVersion.toString())
+}
+
+dependencies {
+    implementation("org.apache.commons:commons-text")
+    val acmeVersion = "11.0.15"
+    implementation("org.eclipse.jetty:jetty-openid:$acmeVersion")
+    implementation("com.google.guava:guava:31.0.1-android")
+    testImplementation("io.kotest:kotest-assertions-shared-js:5.0.0")
+    testImplementation("io.kotest:kotest-assertions-shared-js:5.0.1")
+}
+
+project.plugins.withType(JavaPlugin::class) {
+    dependencies {
+        "errorprone"("com.google.errorprone:error_prone_core:2.9.0")
+    }
+}
+
+application {
+    mainClass.set("deptest.app.AppKt")
+}
